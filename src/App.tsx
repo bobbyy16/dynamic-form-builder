a63de33c0cbe0d4f8416ed557e723fb52342582f
bobbyy16 <bobbyyyyy16@gmail.com>
2025-08-10 16:06:31 +0530

    refactor: reorganize imports and enhance App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import CreateForm from "./pages/CreateForm";
-import Home from "./pages/Home";
-import PreviewForm from "./pages/PreviewForm";
-import ViewAllForms from "./pages/ViewAllForm";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
+import Home from "./pages/Home";
+import CreateForm from "./pages/CreateForm";
+import EditForm from "./pages/EditForm";
+import PreviewForm from "./pages/PreviewForm";
+import ViewAllForms from "./pages/ViewAllForms";
+import Responses from "./pages/Responses";
 
-const App = () => {
+function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<CreateForm />} />
-        <Route path="/preview" element={<PreviewForm />} />
-        <Route path="/myforms" element={<ViewAllForms />} />
-      </Routes>
+      <div className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreateForm />} />
+          <Route path="/edit" element={<EditForm />} />
+          <Route path="/preview" element={<PreviewForm />} />
+          <Route path="/myforms" element={<ViewAllForms />} />
+          <Route path="/responses" element={<Responses />} />
+        </Routes>
+        <Toaster />
+      </div>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
